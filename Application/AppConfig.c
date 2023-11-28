2e894acf1bd1c50d61df0a816d03873a15840d7f
unknown <Davidsobhysabet@gmail.com>
2023-11-29 00:58:30 +0200

    Modified the main AppConfig documentation

--- Application/AppConfig.c
+++ Application/AppConfig.c
@@ -1,9 +1,9 @@
 ﻿
 /*******************************************************************************************************
- *  [FILE NAME]   :      <AppConfig.h>                                                                 *
+ *  [FILE NAME]   :      <AppConfig.c>                                                                 *
  *  [AUTHOR]      :      <David S. Alexander>                                                          *
  *  [DATE CREATED]:      <June 11, 2023>                                                               *
- *  [Description} :      <Header file for application config and include files>                                            *
+ *  [Description} :      <Source file for application config and include files>                        *
  *******************************************************************************************************/
 
 #include "AppConfig.h"
